b3599e84a763546c9b6341a64196dc2016ec27d1
Pau-dna <pasuarezgu@unal.edu.com>
2025-11-22 11:34:36 -0500

    Solo test_homework

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -6,7 +6,7 @@ from ._internals.count_words import count_words
 from ._internals.preprocess_lines import preprocess_lines
 from ._internals.read_all_lines import read_all_lines
 from ._internals.split_into_words import split_into_words
-from ._internals.write_count_words import write_count_words
+from ._internals.write_word_counts import write_count_words
 
 
 def main():
